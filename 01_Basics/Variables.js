303f7bf5616e3edcd697cacd2943d74ef2524112
theyash18 <dakayash7@gmail.com>
2025-07-30 00:10:04 +0530

    changed

--- 01_Basics/Variables.js
+++ 01_Basics/Variables.js
@@ -2,6 +2,7 @@ let UserName="Yash"
 let Password="DakaYash@2006"
 let UserEmail="dakayash7gamil.com"
 let UserState="Gujrat"
+let Name="Dhruv"
 
 
-console.table([UserName,UserEmail,Password,UserState])
+console.table([UserName,UserEmail,Password,UserState,Name])
