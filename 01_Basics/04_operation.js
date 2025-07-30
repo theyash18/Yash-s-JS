89a05eb21cf06decf10f9fc9c698e78c8c80234b
theyash18 <dakayash7@gmail.com>
2025-07-30 22:32:16 +0530

    done

---
+++ 01_Basics/04_operation.js
@@ -0,0 +1,33 @@
+// operations
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue); op=-3
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3); //x is to the power y
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+// let str1 = "Namaste "
+// let str2 = "Coder"
+// console.log(str1 + str2);
+
+// console.log("1" + "2");     12
+// console.log("1" + 2);       12
+// console.log(1 + "2");       12
+// console.log(1 + 2);         3
+// console.log("1" + 2 + 2);   122
+// console.log(1 + 2 + "2");   32
+
+// console.log(true);      true
+// console.log(+true);     1
+// console.log(+"");       0
+
+// let gameCounter = 100;
+// gameCounter++;
+// console.log(gameCounter);       101
+// ++gameCounter;
+// console.log(gameCounter);       102
