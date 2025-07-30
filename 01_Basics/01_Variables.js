fd95fe649429b5c769107ed742f19a96860d3bd0
theyash18 <dakayash7@gmail.com>
2025-07-30 22:11:01 +0530

    Done

--- 01_Basics/01_Variables.js
+++ 01_Basics/01_Variables.js
@@ -1,7 +1,26 @@
-let UserName="Yash"
+/*let UserName="Yash"
 let Password="DakaYash@2006"
 let UserEmail="dakayash7gamil.com"
 let UserState="Gujrat"
-
-
 console.table([UserName,UserEmail,Password,UserState])
+*/
+// Variables: let, const and var
+const accountId = 2324;
+let accountEmail = "coder@google.com";
+var accountPassword = "12345";
+accountCity = "Rishikesh";
+let accountState;
+
+console.table(
+    [accountId, accountEmail, accountPassword, accountCity]
+); 
+
+// accountId = 241; // its not allowed
+
+accountEmail = "nc@gmail.com";
+accountPassword = "54321";
+accountCity = "Bhopal";
+
+console.table(
+    [accountId, accountEmail, accountPassword, accountCity, accountState]
+); 
--- 01_Basics/02_Datatype.js
+++ 01_Basics/02_Datatype.js
@@ -1,5 +1,4 @@
-/* 
-
+/*
 Number => 2 to power 53
 bigint
 string => ""
@@ -7,8 +6,40 @@ boolean => true/false
 null => standaloan Value(object)
 undifined => undifined
 symbol => unique
-
-*/
-
 console.log(typeof undefined) // undifined
 console.log(typeof null) // object
+*/
+
+// data-types and ECMA Standards
+
+"use strict"; // treat all JS code as newer version
+
+let name = "chiranjeev"
+let age = 20
+let isLoggedIn = true
+let state
+// alert(3 + 3) // there is in node have different syntax to use it
+// we're using nodejs, not browser
+
+// console.log(3+3);
+
+// console.log("Namaste");
+
+// datatypes
+// number => 2^53
+// bigInt
+// string = ""
+// boolean => true/false
+// null (its stand-alone value)
+// undefined =>
+// symbol => unique
+
+// object
+// typeof
+
+console.log(typeof "namaste");
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+console.log(typeof null);
+console.log(typeof undefined);
