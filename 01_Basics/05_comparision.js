d261a946b192f5905af6c7dc1c566850bc722ff0
theyash18 <dakayash7@gmail.com>
2025-07-30 23:23:20 +0530

    saved

---
+++ 01_Basics/05_comparision.js
@@ -0,0 +1,29 @@
+// comparison of datatypes
+
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);       true
+// console.log("02" > 1);      false
+
+
+//equlity checkand[==] comparision[<,>,<=,>=]  work differently
+//comparison convert null to a number, treating it as 0.
+
+// console.log(null > 0);       false
+// console.log(null == 0);      false
+// console.log(null >= 0);      true
+// console.log(null <= 0);      true
+
+//  console.log(undefined == 0);       false
+//  console.log(undefined == null);    true
+
+// === strictly check
+//console.log("2" === 2);     //False //convert and check
+//console.log("2" === "2");   //True  // conver and alo check data type
+//console.log("2" === 2);     //False 
