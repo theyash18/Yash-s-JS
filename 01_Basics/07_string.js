6ca9be55404fc27946e0241fff439d30a392dcc2
theyash18 <dakayash7@gmail.com>
2025-07-31 21:53:23 +0530

    done

---
+++ 01_Basics/07_string.js
@@ -0,0 +1,35 @@
+const name = "Yash"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+ const gameName = new String('yash-hc-com')
+
+//  console.log(gameName[4]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('y'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(2,4)
+// console.log(anotherString);
+
+// const newStringOne = "   yash    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://hitesh.com/yash%20daka"
+
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('sundar'))
+
+ console.log(gameName.split('-'));
